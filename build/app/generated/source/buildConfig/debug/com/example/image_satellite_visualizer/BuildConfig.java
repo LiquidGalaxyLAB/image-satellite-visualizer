24d622eb20bbdd701e71855ef8f5b21efb2b3651
Octávio Henrique Müller Barrio <octavio_muller@MacBook-Air-de-Octavio.local>
2021-06-11 00:38:31 -0300

    main commit

---
+++ build/app/generated/source/buildConfig/debug/com/example/image_satellite_visualizer/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.image_satellite_visualizer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.image_satellite_visualizer";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
