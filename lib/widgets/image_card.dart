d76344b842a3c96f1cb83abf3d53a37f9aefa7cb
Octávio Henrique Müller Barrio <octavio_muller@MacBook-Air-de-Octavio.local>
2021-06-25 13:24:29 -0300

    delete images

--- lib/widgets/image_card.dart
+++ lib/widgets/image_card.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:image_satellite_visualizer/models/image_data.dart';
 
-class ImageCard extends StatelessWidget {
+class ImageCard extends StatefulWidget {
   const ImageCard({Key? key, required this.image}) : super(key: key);
   final ImageData image;
+
+  @override
+  _ImageCardState createState() => _ImageCardState();
+}
+
+class _ImageCardState extends State<ImageCard> {
+  Box? imageBox;
+
+  @override
+  void initState() {
+    super.initState();
+    imageBox = Hive.box('imageBox');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +34,7 @@ class ImageCard extends StatelessWidget {
             Expanded(
               flex: 7,
               child: Container(
-                child: Image.network(image.imagePath, fit: BoxFit.cover),
+                child: Image.network(widget.image.imagePath, fit: BoxFit.cover),
               ),
             ),
             Expanded(
@@ -28,7 +42,7 @@ class ImageCard extends StatelessWidget {
               child: Container(
                 padding: EdgeInsets.all(screenSize.width * 0.01),
                 child: Text(
-                  image.title,
+                  widget.image.title,
                   style: TextStyle(
                     color: Theme.of(context).accentColor,
                     fontWeight: FontWeight.bold,
@@ -45,7 +59,7 @@ class ImageCard extends StatelessWidget {
                 padding:
                     EdgeInsets.symmetric(horizontal: screenSize.width * 0.01),
                 child: Text(
-                  image.description,
+                  widget.image.description,
                   style: TextStyle(fontWeight: FontWeight.w300),
                   overflow: TextOverflow.ellipsis,
                   maxLines: 3,
@@ -58,7 +72,9 @@ class ImageCard extends StatelessWidget {
                 child: Row(
                   children: [
                     IconButton(
-                        onPressed: () => print('DELETE'), icon: Icon(Icons.delete)),
+                      onPressed: () => widget.image.delete(),
+                      icon: Icon(Icons.delete),
+                    ),
                     IconButton(
                       onPressed: () => print('EDIT'),
                       icon: Icon(Icons.edit),
@@ -66,8 +82,10 @@ class ImageCard extends StatelessWidget {
                     Spacer(),
                     IconButton(
                       onPressed: () => print('SEND'),
-                      icon: Icon(Icons.send,
-                          color: Theme.of(context).accentColor),
+                      icon: Icon(
+                        Icons.send,
+                        color: Theme.of(context).accentColor,
+                      ),
                     ),
                   ],
                 ),
